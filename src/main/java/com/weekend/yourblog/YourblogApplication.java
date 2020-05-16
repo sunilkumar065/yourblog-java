b8bb256fad6aa5ac1fadc33058c9fae2fb6fb1fe
sunilkumar065 <s55.pragya@gmail.com>
2020-05-16 23:24:46 +0530

    initial commit

---
+++ src/main/java/com/weekend/yourblog/YourblogApplication.java
@@ -0,0 +1,13 @@
+package com.weekend.yourblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weekend/yourblog/YourblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weekend.yourblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
